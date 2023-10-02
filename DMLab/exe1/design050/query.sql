c9df68a6d2088e714fbbf2e1aea776eaa147e1f4
Mohammed Anas <mohammedanas.mca22@rvce.edu.in>
2023-10-02 09:02:41 +0530

    Added Query Two

--- DMLab/exe1/design050/query.sql
+++ DMLab/exe1/design050/query.sql
@@ -1,13 +1,33 @@
 /*
 List the f_Name, l_Name, dept_Name of the employer who draws a salary greater than the average salary of employees working for the Finance department.
 */
-select * from emptbl e left join depttbl d on e.dno=d.dno where salary < (select avg(salary) from emptbl where d.dname='Finance') ;
+select * 
+from emptbl e 
+left join depttbl d 
+	on e.dno=d.dno 
+where salary < (
+	select avg(salary) 
+	from emptbl 
+	where d.dname='Finance'
+) ;
 
 
 
 /*
-List the name and department of the employer who is currently working on more than two project controlled by R&D department.
+List the name and department of the employee who is currently working on more than two project controlled by R&D department.
 */
+select essn,count(*) 
+from workon w 
+left join (
+	select pid,pname,p.dno,d.dname 
+	from prgtbl p 
+	left join depttbl d 
+		on p.dno=d.dno 
+	where dname='R&D' and p.pendate<curdate()
+	) as prg 
+	on w.pid=prg.pid 
+where prg.pname is not null  
+group by essn ;
 
 /*
 List all the ongoing projects controlled by all the departments.
