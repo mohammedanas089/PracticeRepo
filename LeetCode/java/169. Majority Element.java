4b9419cbee5c293e058e2ce3f102973d6b23db32
Mohammed Anas <mohammedanas.mca22@rvce.edu.in>
2023-12-04 13:29:54 +0530

    Monday 04 December 2023 01:29:54 PM IST

---
+++ LeetCode/java/169. Majority Element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+    Map<Integer,Integer> map= new HashMap<>();
+    for(int i:nums){
+        if(map.containsKey(i)){
+            map.put(i,map.get(i)+1);
+        }
+        else{
+            map.put(i,1);
+        }
+    }   
+    for(int i:map.keySet()){
+        if(map.get(i)>(nums.length/2)){
+            return i;
+        }
+    }
+    return -1;
+    }
+}
