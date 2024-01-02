40e081518791263617992826d59c29875ba998a8
Mohammed Anas <mohammedanas.mca22@rvce.edu.in>
2024-01-02 15:23:56 +0530

    Started lab four

---
+++ MAD/LabFour/app/src/main/java/com/example/labfour/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.labfour;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button,button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.button);
+        button2=findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("",String.valueOf("Pressed"));
+                //button.setVisibility(View.GONE);
+                Fragment frag1=new FragmentOne();
+                FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.container,frag1).commit();
+            }
+        });
+
+    }
+}
