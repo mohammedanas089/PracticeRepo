bb0b00ea677708a1d17958d7fd2ae15be2ad1244
Mohammed Anas <mohammedanas.mca22@rvce.edu.in>
2024-01-24 19:55:41 +0530

    Replaced button with image button, Changed Dynamic from java code to xml, added own state representation vector image.

--- MAD/LabOne/app/src/main/java/com/example/labone/MainActivity.java
+++ MAD/LabOne/app/src/main/java/com/example/labone/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.labone;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,29 +19,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView=findViewById(R.id.textView);
-        Button button = findViewById(R.id.button);
+        ImageButton button = findViewById(R.id.button);
         Switch switchButton = findViewById(R.id.switch1);
-        button.setOnTouchListener(new View.OnTouchListener() {
+        ConstraintLayout layout=findViewById(R.id.mylayout);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        showToast("Button Pressed");
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        showToast("Button Released");
-                        break;
-                }
-                return false;
+            public void onClick(View v) {
+                showToast("Button Pressed");
             }
-        });
+        }) ;
 
         switchButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
             button.setEnabled(isChecked);
-            textView.setVisibility(isChecked ? View.VISIBLE : View.INVISIBLE);
             showToast(isChecked ? "Button Enabled" : "Button Disabled");
         });
+
+        TextView comment=new TextView(this);
+        comment.setText("This is a TextView Generated at runtime.\nThe Button changes image based on state dynamically. \nSwitch is used to disable the button");
+        ConstraintLayout.LayoutParams param=new ConstraintLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ConstraintLayout.LayoutParams.MATCH_PARENT
+        );
+        param.setMargins(100,1000, 0, 0);
+        layout.addView(comment,param);
     }
 
     private void showToast(String message) {
